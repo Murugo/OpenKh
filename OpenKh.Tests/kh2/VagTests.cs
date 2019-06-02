dc1d63e4f6300bb00ce4edb1e2262be548c103cd
Rikux3 <barack030@web.de>
2019-06-02 16:24:12 +0200

    Tests for Vag and Vsb files

---
+++ OpenKh.Tests/kh2/VagTests.cs
@@ -0,0 +1,25 @@
+﻿using OpenKh.Kh2;
+using System.IO;
+using Xunit;
+
+namespace OpenKh.Tests.kh2
+{
+    public class VagTests
+    {
+        [Theory]
+        [InlineData("v4.vag")]
+        public void IsVersion4VagFile(string fileName)
+        {
+            Vag vag = new Vag(File.OpenRead($"kh2/res/{fileName}"));
+            Assert.Equal(4, vag.Version);
+        }
+
+        [Theory]
+        [InlineData("v4.vag")]
+        public void IsVolumeOver0(string fileName)
+        {
+            Vag vag = new Vag(File.OpenRead($"kh2/res/{fileName}"));
+            Assert.True(vag.Volume > 0);
+        }
+    }
+}
---
+++ OpenKh.Tests/kh2/VsbTests.cs
@@ -0,0 +1,39 @@
+﻿using OpenKh.Kh2;
+using System.IO;
+using Xunit;
+
+namespace OpenKh.Tests.kh2
+{
+    public class VsbTests
+    {
+        [Theory]
+        [InlineData("test.vsb")]
+        public void HasRightAmountOfEntries(string fileName)
+        {
+            using (var stream = File.OpenRead($"kh2/res/{fileName}"))
+            {
+                Assert.Equal(5, Vsb.Count(stream));
+            }
+        }
+
+        [Theory]
+        [InlineData("test.vsb")]
+        public void ExportAllEntries(string fileName)
+        {
+            using (var stream = File.OpenRead($"kh2/res/{fileName}"))
+            {
+                var dirPath = $"kh2/res/_ex";
+                if (Directory.Exists(dirPath))
+                    Directory.Delete(dirPath);
+
+                Directory.CreateDirectory(dirPath);
+                foreach (var file in Vsb.Open(stream))
+                {
+                    file.ToWav(Path.Join(dirPath, $"{file.StreamName}.wav"));
+                }
+            }
+
+            //get file count from folder, check with count from file
+        }
+    }
+}
